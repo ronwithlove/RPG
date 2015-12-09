7956c8920656b0f4308c3eef8b3423a2b388df59
ronwithlove <33899958@qq.com>
2015-12-09 21:47:39 +0800

    v1.04 镜头跟随

---
+++ Player/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	private Transform player;
+	private Vector3 offset=Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		player=GameObject.FindGameObjectWithTag(Tags.player).transform;
+		offset=transform.position-player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position=player.position+offset;
+	
+	}
+}
