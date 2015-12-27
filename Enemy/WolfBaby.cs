8e7e6d3d910b06358c2ca373531e06988253f87a
ronwithlove <33899958@qq.com>
2015-12-27 15:02:41 +0800

    v1.25 小狼自动攻击

--- Enemy/WolfBaby.cs
+++ Enemy/WolfBaby.cs
@@ -11,19 +11,28 @@ public enum WolfState{
 public class WolfBaby : MonoBehaviour {
 
 	public WolfState state=WolfState.Idel;
-	public float time=1;
+	public float time=1;//每隔1秒从判断巡逻时候的状态
 	public float speed=1;
 	public int hp=100;
-	public float dodgeRage=0.2f;
+	public float dodgeRate=0.2f;
+	public float attack2Rate=0.25f;//25%几率发动攻击2
 	public GameObject HUDTextperfab;//HUDText的perfab
+	public float attackRange;//这个距离的时候可以发起攻击
+	public float targetRange;//超过这个距离小狼失去目标，未超过小狼将移动到attackRange
+	public float attack1AniTime=0.633f;//攻击1播放时间
+	public float attack2AniTime=0.733f;//攻击1播放时间
+	public float attackRate=1f;//攻击率，每隔1秒攻击一次
 
 	private float timer=0;
+	private float attackTimer=0;
 	private bool isWalking=false;
 	private Animation wolfAnim;
 	private CharacterController cc;
 	private Color normalColor;
 	private GameObject HUDTextGo;//新建后的HUDText
 	private Transform beFollowedTarget;
+	private Transform attackTarget;//小狼攻击对象
+
 
 	void Awake(){
 		wolfAnim=this.GetComponent<Animation>();
@@ -70,21 +79,23 @@ public class WolfBaby : MonoBehaviour {
 	}
 
 	public void BeHit(int damage){
-		float value= Random.Range(0f,1f);
-		if(value<dodgeRage){//小狼没被打中
-			HUDTextGo.GetComponent<HUDText>().Add("Miss",Color.gray,1);
-			print ("没被打中");
-			print (HUDTextGo.GetComponent<HUDText>());
-		}else{//被打中了
-			HUDTextGo.GetComponent<HUDText>().Add(-damage,Color.red,1);
-			hp-=damage;
-			StartCoroutine(changeBodyColor());//被打中身体颜色变红 IEnumerator 配合StartCoroutine调用
-			print ("被打中");
-			if(hp<=0){
-				state=WolfState.Death;
-				GameObject.Destroy(this.gameObject,2f);
+		if(state!=WolfState.Death){//不死才执行下面被打
+			float value= Random.Range(0f,1f);
+			if(value<dodgeRate){//小狼没被打中
+				HUDTextGo.GetComponent<HUDText>().Add("Miss",Color.gray,1);
+				print ("没被打中");
+				print (HUDTextGo.GetComponent<HUDText>());
+			}else{//被打中了
+				HUDTextGo.GetComponent<HUDText>().Add(-damage,Color.red,1);
+				hp-=damage;
+				StartCoroutine(changeBodyColor());//被打中身体颜色变红 IEnumerator 配合StartCoroutine调用
+				print ("被打中");
+				if(hp<=0){
+					state=WolfState.Death;
+					GameObject.Destroy(this.gameObject,2f);
+				}
 			}
-		}
+		}//end if(state!=WolfState.Death)
 	}
 
 	IEnumerator changeBodyColor(){//用协程来做
@@ -93,7 +104,41 @@ public class WolfBaby : MonoBehaviour {
 		this.GetComponentInChildren<Renderer>().material.color=normalColor;
 	}
 
+	void AutoAttack(){
+		if(attackTarget!=null){
+			attackTimer+=Time.deltaTime;
+			float distance=Vector3.Distance (attackTarget.position,transform.position);
+			if(distance<=attackRange){//在攻击范围，做攻击处理
+				float value= Random.Range(0f,1f);
+				if(value<attack2Rate){//攻击2
+					wolfAnim.CrossFade("WolfBaby-Attack2");
+					//造成伤害
+					if(attackTimer==attack2AniTime){
+						wolfAnim.CrossFade("WolfBaby-Idle");//动画时间结束就播放空闲动画
+					}
+				}else{//攻击1
+					wolfAnim.CrossFade("WolfBaby-Attack1");
+					//造成伤害
+					if(attackTimer==attack1AniTime){
+						wolfAnim.CrossFade("WolfBaby-Idle");//动画时间结束就播放空闲动画
+					}
+				}
+				if(attackTimer==attackRate){//攻击间隔，现在是设每隔1秒攻击一次
+					attackTimer=0;
+				}
+			}else if(distance>attackRange&& distance<=targetRange){//超出攻击范围，但是在目标范围内。
+				transform.LookAt(attackTarget);
+				cc.SimpleMove(transform.forward*speed);
+				wolfAnim.CrossFade ("WolfBaby-Walk");
+			}else{//超出视线范围
+				attackTarget=null;//失去目标
+			}
+		}else{
+			state=WolfState.Idel;
+		}
+	}
+
 	void OnDestroy() {//MonoBehaviour的函数，当他将被销毁时，调用这个函数
-		GameObject.Destroy(HUDTextGo);//死了以后再造成伤害就不显示了，全完成之后检查一下
+		GameObject.Destroy(HUDTextGo);
 	}
 }
